0097c5459ee4e3dab6f7de4cd20b0d9c4b78a2a2
Nikhilesh Badgu <heenik1999@gmail.com>
2022-09-03 12:48:11 +0530

    Com
    Commit

---
+++ Doctor_Finder/Doctor_Finder/Areas/Users/Controllers/MyUserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctor_Finder.Areas.Users.Controllers
+{
+    [Area("Users")]
+    public class MyUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Doctor_Finder/Doctor_Finder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Doctor_Finder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
